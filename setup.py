c615eda7a59a52a2eb71773b099150ffe7e4ea96
yoiqsram <yoiqrambadian@gmail.com>
2024-02-21 01:46:14 +0700

    configure setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='leantask',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
-        'croniter>=2.0.1',
-        'greenlet>=3.0.3',
-        'python-dateutil>=2.8.2',
-        'pytz>=2023.3.post1',
-        'six>=1.16.0',
-        'SQLAlchemy>=2.0.25',
-        'typing-extensions>=4.9.0'
+        'croniter==2.0.1',
+        'greenlet==3.0.3',
+        'peewee==3.17.1',
+        'python-dateutil==2.8.2',
+        'pytz==2023.3.post1',
+        'six==1.16.0',
+        'typing-extensions==4.9.0'
     ],
     author='Yoiq S Rambadian',
     description='Small flow scheduler'

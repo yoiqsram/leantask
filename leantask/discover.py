d37e3d73cd0a5034841bea9a046b2bdec7dde2ff
yoiqsram <yoiqrambadian@gmail.com>
2024-02-21 17:54:08 +0700

    fix AttributeError on discover

--- leantask/discover.py
+++ leantask/discover.py
@@ -141,7 +141,7 @@ def index_all_flows(
 
             updated_flow_model = (
                 FlowModel.select()
-                .where(FlowModel.path == flow_model.flow_path)
+                .where(FlowModel.path == flow_model.path)
                 .limit(1)
                 [0]
             )

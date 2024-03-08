cd58f959fb960d3fea15edd3f2734cb7625017d9
yoiqsram <yoiqrambadian@gmail.com>
2024-03-08 17:52:36 +0700

    add pre-commit hook

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-VENV_DIR="venv"
-VENV_ACTIVATE_SCRIPT="venv/bin/activate"
+#!/usr/bin/bash
 
-virtualenv venv
+SCRIPT=${BASH_SOURCE[0]}
+SCRIPT_DIR=$(realpath $(dirname $SCRIPT))
+VENV_DIR="$SCRIPT_DIR/venv"
+VENV_ACTIVATE_EXECUTE="$VENV_DIR/bin/activate"
+
+virtualenv "$VENV_DIR"
 if [ $? -ne 0 ]; then
     pip install virtualenv
     if [ $? -ne 0 ]; then
@@ -10,11 +14,13 @@ if [ $? -ne 0 ]; then
     fi
 fi
 
-printf "\n# set required environment variables for the project" >> "$VENV_ACTIVATE_SCRIPT"
-printf "\nexport PATH=$(pwd):$PATH\n" >> "$VENV_ACTIVATE_SCRIPT"
+printf "\n# set required environment variables for the project" >> "$VENV_ACTIVATE_EXECUTE"
+printf "\nPATH=$SCRIPT_DIR:\$PATH" >> "$VENV_ACTIVATE_EXECUTE"
+printf "\nexport PATH\n" >> "$VENV_ACTIVATE_EXECUTE"
 
-source "$VENV_ACTIVATE_SCRIPT"
+source "$VENV_ACTIVATE_EXECUTE"
 pip install -r requirements.txt
+python -m pre_commit install
 
 python -m leantask init
 python -m leantask discover
